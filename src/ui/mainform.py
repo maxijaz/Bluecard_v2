1af46a5d4c9452909a0473301bfa68449f2e97dd
Paul Rogers <paulrogers38@gmail.com>
2025-04-26 16:11:46 +0700

    update lauuncher

--- src/ui/mainform.py
+++ src/ui/mainform.py
@@ -186,6 +186,7 @@ class Mainform(tk.Toplevel):
 
     def on_close(self):
         """Handle Mainform close event."""
+        from src.ui.launcher import Launcher  # Lazy import to avoid circular dependency
         self.destroy()  # Close the Mainform
         Launcher(self.master, self.theme).mainloop()  # Reopen the Launcher
 
